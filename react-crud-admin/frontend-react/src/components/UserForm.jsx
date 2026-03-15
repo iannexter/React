07a0dc6db36c779c69ca1c0604fde4bf12d9d774
Ian Nicolas Notebook <iansistemasbr@gmail.com>
2026-03-14 23:34:28 -0300

    formulario de cadastro

--- react-crud-admin/frontend-react/src/components/UserForm.jsx
+++ react-crud-admin/frontend-react/src/components/UserForm.jsx
@@ -2,7 +2,38 @@
 export const UserForm = () => {
 
     return (
-<form action=""></form>
+        <form >
+
+            <input
+                className="form-control my-3 w-75"
+                placeholder="Username"
+                name="username" />
+
+            <input
+                className="form-control my-3 w-75"
+                placeholder="Password"
+                name="password" />
+
+            <input
+                className="form-control my-3 w-75"
+                placeholder="Email"
+                name="email" />
+
+
+            <button
+
+                className="btn btn-primary"
+                type="submit"
+
+            >
+
+
+                Criar
+
+            </button>
+
+
+        </form>
 
     );
 
