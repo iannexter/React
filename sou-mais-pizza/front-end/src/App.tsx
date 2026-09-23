b1cbc760d80db7d2a85bd07d9e4da02260b3ed0e
Ian Nicolas <iansistemasbr@gmail.com>
2026-09-22 21:50:33 -0300

    Front-end Pizzaria React tailwindcss

---
+++ sou-mais-pizza/front-end/src/App.tsx
@@ -0,0 +1,14 @@
+//Componente é uma função que retorna um html
+
+//exemplo de componente:
+const App = () =>{
+
+  return(
+
+    <p className="bg-red-500">Pizza</p>
+
+  )
+}
+
+//agora exportamos o componente:
+export default App;
