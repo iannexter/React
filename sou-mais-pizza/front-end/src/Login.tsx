0f2b06e8889f02b8eaaee44f9f33959f9a0109f8
Ian Nicolas <iansistemasbr@gmail.com>
2026-09-24 21:29:47 -0300

    Arrumando componente de login para FORM

--- sou-mais-pizza/front-end/src/Login.tsx
+++ sou-mais-pizza/front-end/src/Login.tsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+
 import Input from "./components/Input";
 
+
+
+
 const Login = () => {
+
+
+ //email --> variavel setEmail --> valor para mudar a variavel 
+  const [email, setEmail] = useState("email@email.com");
+
+
+  const [password, setPassword] = useState("");
+
+  function changeName() {
+
+    console.log(email)
+
+  }
+
+    // function handleSubmit(e) {
+    //   // e.preventDefault();
+    //   console.log(email);
+    //   console.log(password);
+    // }
+
+
   return (
-    <div className="flex h-screen items-center justify-center bg-[#161410]">
+    <form className="flex h-screen items-center justify-center bg-[#161410]"
+    
+    onSubmit= { (e) => {
+
+      e.preventDefault();
+
+          console.log(email);
+          console.log(password);
+
+
+    }
+  }
+
+
+    >
       <div className="flex w-[350px] flex-col items-stretch gap-2">
         <img
           src="/logo.png"
@@ -10,19 +50,37 @@ const Login = () => {
           className="mx-auto mb-4 h-[200px] w-[200px]"
         />
 
-        <Input placeholder="E-mail" />
+        <Input placeholder="E-mail" 
+        onChange={(e) => setEmail(e.target.value)}/>
 
-        <Input placeholder="Senha" />
+        <Input placeholder="Senha" 
 
-        <button className="w-full cursor-pointer rounded-md bg-[#C92A0E] py-2 text-sm font-bold text-white">
+        type="password"
+        
+        onChange = {(e) => setPassword(e.target.value)}
+        
+        />
+
+       
+
+        <button className="w-full cursor-pointer rounded-md bg-[#C92A0E] py-2 text-sm font-bold text-white"
+        
+        
+
+
+        >
           Login
         </button>
 
 
         
       </div>
-    </div>
+    </form>
   );
 };
 
 export default Login;
+
+
+
+ // <p className="text-white text-center" >{email}</p>
